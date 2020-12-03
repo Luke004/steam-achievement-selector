c4182711a004bffd979a28d80fa8b240826835d7
Lukas <lukas.hilfrich@mni.thm.de>
2020-12-03 20:22:02 +0100

    add loading and saving of the json files needed

---
+++ src/ContentPanel.java
@@ -0,0 +1,234 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+
+import javax.swing.*;
+
+public class ContentPanel extends JPanel implements ActionListener {
+
+    JLabel picture, loadingText;
+
+    public ContentPanel() throws Exception {
+        super(new BorderLayout());
+        try {
+            Util.readJson("ownedGames");    // just attempt to read it, if it fails -> catch
+            JSONObject achievementsList = (JSONObject) Util.readJson("achievementsList");
+            initComboBox(achievementsList);
+        } catch (Exception e) {
+            // could not load json from persisted files
+            loadingText = new JLabel();
+            loadingText.setFont(loadingText.getFont().deriveFont(Font.ITALIC));
+            loadingText.setHorizontalAlignment(JLabel.CENTER);
+            loadingText.setBorder(BorderFactory.createEmptyBorder(10,0,0,0));
+            add(loadingText, BorderLayout.CENTER);
+            // load all owned games
+            Thread loadOwnedGames = new Thread(() -> {
+                loadingText.setText("Loading all owned games with achievements ...");
+                try {
+                    Util.writeJson(Util.getAllOwnedGames(), "ownedGames");
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            });
+            loadOwnedGames.start();
+            loadOwnedGames.join();
+            // when here, we have finished loading of all owned games
+            // now load achievement info for each game:
+            Thread loadAchievementInfo = new Thread(() -> {
+                loadingText.setText("Loading achievement info for each game ...");
+                try {
+                    JSONObject ownedGamesJSON = (JSONObject) Util.readJson("ownedGames");
+                    JSONObject responseJSON = (JSONObject) ownedGamesJSON.get("response");
+                    JSONArray gamesJSON = (JSONArray) responseJSON.get("games");
+                    Long gameCount = (Long) responseJSON.get("game_count");
+                    //ArrayList<String> listOfGamesWithAchievements = new ArrayList<>();
+                    JSONObject achievements_root_list = new JSONObject();
+                    for (int i = 0; i < gameCount; ++i) {
+                        JSONObject game = ((JSONObject) gamesJSON.get(i));
+                        Boolean has_community_visible_stats = (Boolean) game.get("has_community_visible_stats");
+                        if (has_community_visible_stats != null && has_community_visible_stats) {
+                            JSONObject achievements_root = Util.getAchievementByAppID((Long) game.get("appid"));
+                            JSONObject playerStats = (JSONObject) achievements_root.get("playerstats");
+                            JSONArray achievements = (JSONArray) playerStats.get("achievements");
+                            if (achievements != null) {
+                                achievements_root_list.put(game.get("appid"), playerStats);
+                            }
+                        }
+                    }
+                    Util.writeJson(achievements_root_list, "achievementsList");
+                    // finally load the combo box using the newly created achievement list
+                    JSONObject achievementsList = (JSONObject) Util.readJson("achievementsList");
+                    initComboBox(achievementsList);
+                    loadingText.setVisible(false);
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            });
+            loadAchievementInfo.start();
+        }
+
+        /*
+        String[] petStrings = { "Bird", "Cat", "Dog", "Rabbit", "Pig" };
+
+        //Create the combo box, select the item at index 4.
+        //Indices start at 0, so 4 specifies the pig.
+        JComboBox petList = new JComboBox(gameNames);
+        petList.setSelectedIndex(4);
+        petList.addActionListener(this);
+
+        //Set up the picture.
+        picture = new JLabel();
+        picture.setFont(picture.getFont().deriveFont(Font.ITALIC));
+        picture.setHorizontalAlignment(JLabel.CENTER);
+        updateLabel(petStrings[petList.getSelectedIndex()]);
+        picture.setBorder(BorderFactory.createEmptyBorder(10,0,0,0));
+
+        //The preferred size is hard-coded to be the width of the
+        //widest image and the height of the tallest image + the border.
+        //A real program would compute this.
+        picture.setPreferredSize(new Dimension(177, 122+10));
+
+        //Lay out the demo.
+        add(petList, BorderLayout.PAGE_START);
+        add(picture, BorderLayout.PAGE_END);
+        setBorder(BorderFactory.createEmptyBorder(20,20,20,20));
+         */
+    }
+
+    private void initComboBox(JSONObject achievementsList) {
+        try {
+            ArrayList<String> listOfGamesWithAchievements = new ArrayList<>();
+
+            for(Object key : achievementsList.keySet()){
+                JSONObject playerStats = ((JSONObject) achievementsList.get(key));
+                JSONArray achievements = (JSONArray) playerStats.get("achievements");
+
+
+                String gameInfo = (String) playerStats.get("gameName");
+                int achieved_counter = 0;
+                for (Object achievement : achievements) {
+                    Long achieved = (Long) ((JSONObject) achievement).get("achieved");
+                    if (achieved == 1) {
+                        achieved_counter++;
+                    }
+                }
+                gameInfo += " (" + achieved_counter + "/" + achievements.size() + ")";
+
+                listOfGamesWithAchievements.add(gameInfo);
+
+            }
+            JComboBox gameList = new JComboBox(listOfGamesWithAchievements.toArray());
+            //petList.setSelectedIndex(0);
+            gameList.addActionListener(this);
+            //System.out.println(listOfGamesWithAchievements.size());
+            add(gameList, BorderLayout.PAGE_START);
+            setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+
+            /*
+            JSONObject responseJSON = (JSONObject) ownedGamesJSON.get("response");
+            JSONArray gamesJSON = (JSONArray) responseJSON.get("games");
+            Long gameCount = (Long) responseJSON.get("game_count");
+            ArrayList<String> listOfGamesWithAchievements = new ArrayList<>();
+            for (int i = 0; i < gameCount; ++i) {
+                JSONObject game = ((JSONObject) gamesJSON.get(i));
+                Boolean has_community_visible_stats = (Boolean) game.get("has_community_visible_stats");
+                if (has_community_visible_stats != null && has_community_visible_stats) {
+                    JSONObject achievements_root = (JSONObject) achievementsList.get( game.get("appid"));
+                    //JSONObject achievements_root = Util.getAchievementByAppID((Long) game.get("appid"));
+                    JSONObject playerStats = (JSONObject) achievements_root.get("playerstats");
+                    JSONArray achievements = (JSONArray) playerStats.get("achievements");
+                    if (achievements != null) {
+                        String gameInfo = (String) game.get("name");
+                        int achieved_counter = 0;
+                        for (Object achievement : achievements) {
+                            Long achieved = (Long)((JSONObject) achievement).get("achieved");
+                            if(achieved == 1){
+                                achieved_counter++;
+                            }
+                        }
+                        gameInfo += " (" + achieved_counter + "/" + achievements.size() + ")";
+
+                        listOfGamesWithAchievements.add(gameInfo);
+                        int debug = 0;
+                    }
+                }
+            }
+            JComboBox gameList = new JComboBox(listOfGamesWithAchievements.toArray());
+            //petList.setSelectedIndex(0);
+            gameList.addActionListener(this);
+            //System.out.println(listOfGamesWithAchievements.size());
+            add(gameList, BorderLayout.PAGE_START);
+            setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+             */
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Listens to the combo box.
+     */
+    public void actionPerformed(ActionEvent e) {
+        JComboBox cb = (JComboBox) e.getSource();
+        String petName = (String) cb.getSelectedItem();
+        updateLabel(petName);
+    }
+
+    protected void updateLabel(String name) {
+        ImageIcon icon = createImageIcon("images/" + name + ".gif");
+        picture.setIcon(icon);
+        picture.setToolTipText("A drawing of a " + name.toLowerCase());
+        if (icon != null) {
+            picture.setText(null);
+        } else {
+            picture.setText("Image not found");
+        }
+    }
+
+    /**
+     * Returns an ImageIcon, or null if the path was invalid.
+     */
+    protected static ImageIcon createImageIcon(String path) {
+        java.net.URL imgURL = ContentPanel.class.getResource(path);
+        if (imgURL != null) {
+            return new ImageIcon(imgURL);
+        } else {
+            System.err.println("Couldn't find file: " + path);
+            return null;
+        }
+    }
+
+    /**
+     * Create the GUI and show it.  For thread safety,
+     * this method should be invoked from the
+     * event-dispatching thread.
+     */
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("Steam Achievement Selector");
+        frame.setPreferredSize(new Dimension(400, 300));
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        try {
+            // create and set up the content pane
+            JComponent newContentPane = new ContentPanel();
+            newContentPane.setOpaque(true); //content panes must be opaque
+            frame.setContentPane(newContentPane);
+            frame.pack();
+            frame.setVisible(true);
+        } catch (Exception e) {
+            System.out.println("Failed to build content pane!");
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(ContentPanel::createAndShowGUI);
+    }
+}
---
+++ src/Util.java
@@ -0,0 +1,59 @@
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Util {
+
+    private static final File directory = new File("persisted-data/");
+    private static final String USER_AGENT = "Mozilla/5.0";
+
+    public static JSONObject getAllOwnedGames() throws Exception {
+        String url = "https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/" +
+                "?key=2321BD719D46F6E288C8ACDDA8C4AF02&steamid=76561198009775508&include_appinfo=1&include_played_free_games=1";
+        String response = getRequest(url);
+        return (JSONObject) new JSONParser().parse(response);
+    }
+
+    public static JSONObject getAchievementByAppID(long appID) throws Exception {
+        String url = "https://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v1/" +
+                "?key=2321BD719D46F6E288C8ACDDA8C4AF02&steamid=76561198009775508&appid=" + appID;
+        String response = getRequest(url);
+        return (JSONObject) new JSONParser().parse(response);
+    }
+
+    private static String getRequest(String url) throws Exception {
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestProperty("User-Agent", USER_AGENT);
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuilder response = new StringBuilder();
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+        return response.toString();
+    }
+
+    public static Object readJson(String filename) throws Exception {
+        File m_file = new File(directory + File.separator + filename);
+        FileReader reader = new FileReader(m_file);
+        JSONParser jsonParser = new JSONParser();
+        return jsonParser.parse(reader);
+    }
+
+    public static void writeJson(JSONObject mJSONObject, String fileName) {
+        directory.mkdirs();
+        File m_file = new File(directory + File.separator + fileName);
+        try (FileWriter fileWriter = new FileWriter(m_file)) {
+            fileWriter.write(mJSONObject.toJSONString());
+            fileWriter.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
